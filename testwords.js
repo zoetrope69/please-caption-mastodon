bb11ce8d81f2d5faf9acd7a500c7bac2d22d0e43
Glitch (goosebumps) <none>
2018-08-19 21:29:19 +0000

    🚠🔋 Checkpoint ./testwords.js:267931/1066 ./js/grammar.json:267931/18

--- testwords.js
+++ testwords.js
@@ -2,23 +2,25 @@ const { words } = require('./js/grammar.json')
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const word = 'somethingthatwouldnevercomebackwithanything'
-// const word = 'dog'
+console.log(words.length)
+// const wordToGetTo = 900
+// words.slice(wordToGetTo - 300, wordToGetTo).forEach(word => {
+//   fetch(`https://loremflickr.com/1/1/${word}`).then(response => {
+//     console.log(response.url)
+//     if (response.url.includes('defaultImage')) {
+//       if (word === words[wordToGetTo]) console.log(word + ' Didnt find image')
 
-fetch(`https://loremflickr.com/1/1/${word}`).then(response => {
-  console.log(response.url)
-  if (response.url.includes('defaultImage')) {
-    console.log(word + ' Didnt find image')
-    
-    fs.appendFile('wordsthatdidntwork.txt', word + "\n", function (err) {
-      if (err) {
-        console.error(err)
-      }
-    })
-    
-    return
-  }
+//       fs.appendFile('wordsthatdidntwork.txt', '"' + word + '",' + "\n", function (err) {
+//         if (err) {
+//           console.error(err)
+//         }
+//       })
 
-  console.log(word + ' Found image')
-})
+
+//       return
+//     }
+
+//  if (word === words[wordToGetTo]) console.log(word + ' Found image')
+//   })
+// })
 
