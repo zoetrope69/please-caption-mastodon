b23f7b5033cedc52f64c440e35ea222c400c953b
Glitch (goosebumps) <none>
2018-08-19 21:39:37 +0000

    ☝️🎡 Checkpoint ./testwords.js:267931/754

--- testwords.js
+++ testwords.js
@@ -2,15 +2,18 @@ const { words } = require('./js/grammar.json')
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-console.log(words.length)
-// const wordToGetTo = 900
-// words.slice(wordToGetTo - 300, wordToGetTo).forEach(word => {
-//   fetch(`https://loremflickr.com/1/1/${word}`).then(response => {
-//     console.log(response.url)
-//     if (response.url.includes('defaultImage')) {
-//       if (word === words[wordToGetTo]) console.log(word + ' Didnt find image')
+const wordDidntWork = require('wordsthatdidntwork.json')
+console.log(wordDidntWork[0])
 
-//       fs.appendFile('wordsthatdidntwork.txt', '"' + word + '",' + "\n", function (err) {
+// console.log(words.length)
+// const wordToGetTo = 1956
+// console.log(words[wordToGetTo])
+// words.slice(wordToGetTo - 300, wordToGetTo + 1).forEach(word => {
+//   fetch(`https://loremflickr.com/1/1/${word}`).then(response => {
+//     if (response.url.includes('defaultImage')) {
+//       if (word === words[wordToGetTo ]) console.log(word + ' Didnt find image')
+
+//       fs.appendFile('wordsthatdidntwork.json', '"' + word + '",' + "\n", function (err) {
 //         if (err) {
 //           console.error(err)
 //         }
@@ -19,8 +22,7 @@ console.log(words.length)
 
 //       return
 //     }
-
-//  if (word === words[wordToGetTo]) console.log(word + ' Found image')
-//   })
+//    if (word === words[wordToGetTo]) console.log(word + ' Found image')
+//   }).catch(console.error)
 // })
 
