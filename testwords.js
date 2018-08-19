5b94dd0ecaa49cef6d939a0f36622585da3e4e02
Glitch (goosebumps) <none>
2018-08-19 21:47:00 +0000

    🌱🚁 Checkpoint

--- testwords.js
+++ testwords.js
@@ -2,27 +2,18 @@ const { words } = require('./js/grammar.json')
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const wordDidntWork = require('wordsthatdidntwork.json')
-console.log(wordDidntWork[0])
+const wordDidntWork = require(__dirname + '/wordsthatdidntwork.json')
 
-// console.log(words.length)
-// const wordToGetTo = 1956
-// console.log(words[wordToGetTo])
-// words.slice(wordToGetTo - 300, wordToGetTo + 1).forEach(word => {
-//   fetch(`https://loremflickr.com/1/1/${word}`).then(response => {
-//     if (response.url.includes('defaultImage')) {
-//       if (word === words[wordToGetTo ]) console.log(word + ' Didnt find image')
-
-//       fs.appendFile('wordsthatdidntwork.json', '"' + word + '",' + "\n", function (err) {
-//         if (err) {
-//           console.error(err)
-//         }
-//       })
-
-
-//       return
-//     }
-//    if (word === words[wordToGetTo]) console.log(word + ' Found image')
-//   }).catch(console.error)
-// })
+words.forEach((word, i) => {
+    if (!wordDidntWork.includes(word)) {
+      fs.appendFile('wordsthatdidwork.json', '"' + word + '",' + "\n", function (err) {
+        if (err) {
+          console.error(err)
+        }
+      })
+    }
+  console.log(i)
+  
+  if (i === words.length) console.log('done')
+})
 
